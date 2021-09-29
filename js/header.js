d26bd889080768f6d4d91bb9631493419a35744a
taran3395 <46003621+taran3395@users.noreply.github.com>
2021-09-29 12:40:33 +0330

    toolbar-wrapper

--- js/header.js
+++ js/header.js
@@ -1,39 +1,47 @@
-let header_hamIco = document.querySelector('.header_hamIco');
-let header_line = document.querySelector('.header_line');
-let header_popup = document.querySelector('.header_popup');
-let header_close = document.querySelector('.header_close');
-let toolBar_logo = document.querySelector('.toolBar_logo');
-let body = document.querySelector('body');
+let toolBar_hamIco = document.querySelector(".toolBar_hamIco");
+let toolBar_line = document.querySelector(".toolBar_line");
+let toolBar_popup = document.querySelector(".toolBar_popup");
+let toolBar_close = document.querySelector(".toolBar_close");
+let toolBar_logo = document.querySelector(".toolBar_logo");
+let body = document.querySelector("body");
 let menu_is_open = false;
 
-header_hamIco.addEventListener('click' , ()=>{
-    header_popup.classList.toggle('header_popup--open');
-    header_line.classList.toggle('header_line--open');
-    header_hamIco.classList.toggle('header_hamIco--open');
-    header_close.classList.toggle('header_close--open');
-    menu_is_open = true;
-})
-header_popup.addEventListener('click' , (e)=>{
-    e.stopPropagation();
-    e.preventDefault();
-})
-header_close.addEventListener('click' , ()=>{
-    if (menu_is_open = true){
-        header_popup.classList.remove('header_popup--open');
-        header_line.classList.remove('header_line--open');
-        header_hamIco.classList.remove('header_hamIco--open');
-        header_close.classList.toggle('header_close--open');
-        menu_is_open = false;
+toolBar_hamIco.addEventListener("click", () => {
+  toolBar_popup.classList.toggle("toolBar_popup--open");
+  toolBar_line.classList.toggle("toolBar_line--open");
+  toolBar_hamIco.classList.toggle("toolBar_hamIco--open");
+  toolBar_close.classList.toggle("toolBar_close--open");
+  menu_is_open = true;
+});
+toolBar_popup.addEventListener("click", (e) => {
+  e.stopPropagation();
+  e.preventDefault();
+});
+toolBar_close.addEventListener(
+  "click",
+  () => {
+    if ((menu_is_open = true)) {
+      toolBar_popup.classList.remove("toolBar_popup--open");
+      toolBar_line.classList.remove("toolBar_line--open");
+      toolBar_hamIco.classList.remove("toolBar_hamIco--open");
+      toolBar_close.classList.toggle("toolBar_close--open");
+      menu_is_open = false;
     }
-}, true)
-window.addEventListener('scroll' , ()=>{
-    if (menu_is_open = true){
-        header_popup.classList.remove('header_popup--open');
-        header_line.classList.remove('header_line--open');
-        header_hamIco.classList.remove('header_hamIco--open');
-        header_close.classList.toggle('header_close--open');
-        menu_is_open = false;
+  },
+  true
+);
+window.addEventListener(
+  "scroll",
+  () => {
+    if ((menu_is_open = true)) {
+      toolBar_popup.classList.remove("toolBar_popup--open");
+      toolBar_line.classList.remove("toolBar_line--open");
+      toolbar_hamIco.classList.remove("toolBar_hamIco--open");
+      toolBar_close.classList.toggle("toolBar_close--open");
+      menu_is_open = false;
     }
-}, true)
+  },
+  true
+);
 
 //index
