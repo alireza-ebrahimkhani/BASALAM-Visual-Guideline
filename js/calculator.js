24aa49bb7496c14026bd89fba3352385f3546986
Kasra Ghavidel <32060084+kasraghavidel@users.noreply.github.com>
2021-12-20 18:43:38 +0330

    a

--- js/calculator.js
+++ js/calculator.js
@@ -23,15 +23,15 @@ calculator_submit.addEventListener('click' , ()=>{
         sSide = calculator_height;
     }
     let hiUnit=sSide * 0.07;
-    calculator_item[0].innerHTML = Math.floor(hiUnit * 1);
-    calculator_item[1].innerHTML = Math.floor(hiUnit * 1.75);
-    calculator_item[2].innerHTML = Math.floor(hiUnit * 1.2);
-    calculator_item[3].innerHTML = Math.floor(hiUnit * 0.8);
-    calculator_item[4].innerHTML = Math.floor(hiUnit * 0.65); 
-    calculator_item[5].innerHTML = Math.floor(hiUnit * 0.45);
-    calculator_item[6].innerHTML = Math.floor(hiUnit * 0.35);
-    calculator_item[7].innerHTML = Math.floor(hiUnit * 0.6);
-    calculator_item[8].innerHTML = Math.floor(hiUnit * 0.35);
-    calculator_item[9].innerHTML = Math.floor(hiUnit * 0.25);
-    calculator_item[10].innerHTML = Math.floor(hiUnit * 0.20);
+    calculator_item[0].innerHTML = Math.floor(hiUnit * 1) + " pt";
+    calculator_item[1].innerHTML = Math.floor(hiUnit * 1.75) + " pt";
+    calculator_item[2].innerHTML = Math.floor(hiUnit * 1.2) + " pt";
+    calculator_item[3].innerHTML = Math.floor(hiUnit * 0.8) + " pt";
+    calculator_item[4].innerHTML = Math.floor(hiUnit * 0.65) + " pt";
+    calculator_item[5].innerHTML = Math.floor(hiUnit * 0.45) + " pt";
+    calculator_item[6].innerHTML = Math.floor(hiUnit * 0.35) + " pt";
+    calculator_item[7].innerHTML = Math.floor(hiUnit * 0.6) + " pt";
+    calculator_item[8].innerHTML = Math.floor(hiUnit * 0.35) + " pt";
+    calculator_item[9].innerHTML = Math.floor(hiUnit * 0.25) + " pt";
+    calculator_item[10].innerHTML = Math.floor(hiUnit * 0.20) + " pt";
 });
